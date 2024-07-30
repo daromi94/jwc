6614ef2f69c0d933e1e8e1341abc358a4ae0b073
daromi94 <davromero07@gmail.com>
2024-07-30 11:26:06 -0500

    feat: :sparkles: count bytes

--- src/main/scala/com/jwc/Cli.scala
+++ src/main/scala/com/jwc/Cli.scala
@@ -1,15 +1,42 @@
 package com.jwc
 
+val USAGE = "usage: jwc [-clw] [file ...]"
+
 @main def main(args: String*): Unit =
   if args.size < 1 then
-    Console.out.println("usage: jwc [-clw] [file ...]")
-    sys.exit(1)
+    Console.err.println(USAGE)
+    sys.exit(2)
 
-  val options = parseOptions(args(0))
-  options.foreach(println)
+  val countOptions = parseCountOptions(args(0))
+
+  val filePaths = parseFilePaths(args.toSeq)
+  if filePaths.isEmpty then
+    Console.err.println(USAGE)
+    sys.exit(2)
+
+  val counts = filePaths.flatMap(p => countFile(p, countOptions))
+
+  // TODO: print to stdout
+  println(counts)
+
   sys.exit(0)
 
-def parseOptions(arg: String): Set[CountOption] =
+def parseCountOptions(arg: String): Set[CountOption] =
+  // TODO: validate options
   if arg.startsWith("-") then
     arg.substring(1).toSet.flatMap(c => CountOption.from(c))
   else Set(CountOption.Bytes, CountOption.Lines, CountOption.Words)
+
+def parseFilePaths(args: Seq[String]): Seq[FilePath] =
+  // TODO: validate paths
+  args.filter(a => !a.startsWith("-"))
+
+def countFile(
+    filePath: FilePath,
+    countOptions: Set[CountOption]
+): Option[(FilePath, Count)] =
+  val countWithOptions = (b: Array[Byte]) => count(b, countOptions)
+
+  readBytesAndApply(filePath, countWithOptions) match
+    case Some(c) => Some((filePath, c))
+    case None    => None // TODO: handle i/o exceptions gracefully
--- src/main/scala/com/jwc/Counters.scala
+++ src/main/scala/com/jwc/Counters.scala
@@ -1,5 +1,7 @@
 package com.jwc
 
+type CountValue = Long
+
 enum CountOption(val value: Char):
   case Bytes extends CountOption('c')
   case Lines extends CountOption('l')
@@ -8,3 +10,31 @@ enum CountOption(val value: Char):
 object CountOption:
   def from(value: Char): Option[CountOption] =
     CountOption.values.find(o => o.value == value)
+
+type Count = Map[CountOption, CountValue]
+
+def count(
+    bytes: Array[Byte],
+    countOptions: Set[CountOption]
+): Count =
+  def delegate(
+      countOption: CountOption,
+      bytes: Array[Byte]
+  ): (CountOption, CountValue) =
+    countOption match
+      case CountOption.Bytes => (countOption, countBytes(bytes))
+      case CountOption.Lines => (countOption, countLines(bytes))
+      case CountOption.Words => (countOption, countWords(bytes))
+
+  countOptions.map(o => delegate(o, bytes)).map((o, v) => o -> v).toMap
+
+def countBytes(bytes: Array[Byte]): CountValue =
+  bytes.length
+
+def countLines(bytes: Array[Byte]): CountValue =
+  // REVIEW
+  bytes.count(b => b == 10)
+
+def countWords(bytes: Array[Byte]): CountValue =
+  // FIXME: ignores consecutive whitespaces
+  bytes.count(b => b == 32) + 1
